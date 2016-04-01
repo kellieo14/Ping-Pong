9e05246f5cf789ba8a3a612076e994a424b40e98
Kellie Olson <kellie.a.olson@biola.edu>
2016-04-01 14:28:18 -0700

    change some layout of numberArray and pingPong

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,38 @@
-var result = [];
-var calculation = function(inputNumber) {
+var numberArray = function(inputNumber) {
+    var resultArray = [];
+    for (i=1; i<=inputNumber; i++) {
+       resultArray.push(i);
+    }
+    return resultArray;
+}
+
+
+var pingPong = function (inputNumber) {
+  var three = (inputNumber % 3);
+  var five = (inputNumber % 5);
+  var fifteen = (inputNumber % 15);
   for (i=0; i<=inputNumber; i++) {
-     result += [i];
-    // debugger;
-      }
-//     var three = (inputNumber % 3);
-//     var five = (inputNumber % 5);
-//     var fifteen = (inputNumber % 15);
-//     var result = inputNumber[i];
-//   }
-//     if (fifteen === 0) {
-//     var result = "pingpong";
-//   } else if (five === 0) {
-//     var result = "pong";
-//   } else if (three === 0) {
-//     var result = "ping";
-//   } else {
-//     var result = inputNumber(i);
-// }
+
+      if (fifteen === 0) {
+      var result = "pingpong";
+    } else if (five === 0) {
+      var result = "pong";
+    } else if (three === 0) {
+      var result = "ping";
+    } else {
+      var result = i-1;
+      console.log(inputNumber);
+    }
+  }
   return result;
-};
+}
 
 $(document).ready(function() {
   $("form").submit(function(event) { // takes the user input
     event.preventDefault();
     var formInput = parseInt($("#input").val());
-    var final = calculation(formInput);
+    var array = numberArray(formInput);
+    var final = pingPong(formInput);
     $(".output").text(final);
   });
 });
