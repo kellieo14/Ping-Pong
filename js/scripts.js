1e8af2d1cf9d59de78beb96e2a86540c948acf96
Kellie Olson <kellie.a.olson@biola.edu>
2016-04-01 08:19:26 -0700

    add basic for loop with ping, pong and pingpong if statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
+var calculation = function(inputNumber) {
+  for (i=0; i<inputNumber; i++) {
+    var three = (inputNumber % 3);
+    var five = (inputNumber % 5);
+    var fifteen = (inputNumber % 15);
+  }
+    if (fifteen === 0) {
+    var result = "pingpong";
+  } else if (five === 0) {
+    var result = "pong";
+  } else if (three === 0) {
+    var result = "ping";
+  } else {
+    var result = inputNumber(i);
+}
+  return result;
+};
 
 $(document).ready(function() {
   $("form").submit(function(event) { // takes the user input
