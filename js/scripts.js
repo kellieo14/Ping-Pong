356cd155982cbd10f983badb96d19935fca3aadf
Kellie Olson <kellie.a.olson@biola.edu>
2016-04-01 15:06:28 -0700

    change pingPong to run numberArray for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,33 @@
 var numberArray = function(inputNumber) {
     var resultArray = [];
-    for (i=1; i<=inputNumber; i++) {
+    for (i = 1; i <= inputNumber; i++) {
        resultArray.push(i);
     }
     return resultArray;
 }
 
 
-var pingPong = function (inputNumber) {
-  var three = (inputNumber % 3);
-  var five = (inputNumber % 5);
-  var fifteen = (inputNumber % 15);
-  for (i=0; i<=inputNumber; i++) {
-
-      if (fifteen === 0) {
-      var result = "pingpong";
-    } else if (five === 0) {
-      var result = "pong";
-    } else if (three === 0) {
+var pingPong = function (array) {
+  var result;
+  for (i=0; i<=array.length; i++) {
+      if (array[i] % 15 === 0) {
+      result = "pingpong";
+    } else if (array[i] % 5 === 0) {
+      result = "pong";
+    } else if (array [i] % 3 === 0) {
       var result = "ping";
     } else {
-      var result = i-1;
-      console.log(inputNumber);
+      result = i + 1;
     }
+    $(".output").append($("<li>").text(result));
   }
-  return result;
 }
 
 $(document).ready(function() {
-  $("form").submit(function(event) { // takes the user input
-    event.preventDefault();
+  $("button").click(function() {
     var formInput = parseInt($("#input").val());
     var array = numberArray(formInput);
-    var final = pingPong(formInput);
-    $(".output").text(final);
+  pingPong(formInput);
+  pingPong(array);
   });
 });
